890fa389ac6da24dd45a514c023cf5d20981df9b
David Johnson <djcomlab@gmail.com>
2019-05-30 12:30:56 +0100

    Updates with new data snapshot and uses latest ISA-API

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='isamemorystudy',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.2.0',
     description='A small study on the memory footprint size used by ISA-API for experimental DAGs.',
     author='David Johnson',
     license='MIT',
